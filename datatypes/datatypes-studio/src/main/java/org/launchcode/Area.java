7227181c48971cb2357387e1289ec8a7297d81a7
anitha-gk <139910108+anitha-gk@users.noreply.github.com>
2023-08-28 17:20:38 -0500

    Studio 1 Datatype

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+//.*;
+import java.util.Scanner ;
+public class Area {
+    public static void main(String[] args)
+    {
+       Scanner sc= new Scanner(System.in);
+       System.out.println("Enter the radius of the circle:");
+       double radius=sc.nextDouble();
+       double area = Math.PI * radius *radius;
+       System.out.println("The area of a circle of radius:" + area);
+
+    }
+}
